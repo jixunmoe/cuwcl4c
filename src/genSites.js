c7718e4c1e6b973fa45cae0ae8ea084c4d9017eb
JixunMoe <jixun.moe@gmail.com>
2014-10-05 21:33:50 +0100

    Add missing files

---
+++ src/genSites.js
@@ -0,0 +1,20 @@
+var modDir = __dirname + '/host/';
+var fs = require ('fs');
+
+var ret = [];
+fs.readdirSync (modDir).forEach (function (m) {
+	if (m.slice(-3) === '.js') {
+		var file = fs.readFileSync (modDir + m).toString ();
+		var name = file.match (/name\s*:\s*'([^']+)/);
+		name = name ? name[1] : m.slice(-3);
+		var hosts = file.match (/host\s*:\s*(\[.+?\]|'[^']+)/)[1].replace(/'/g, '').replace (/\/\*[\s\S]+\*\//g, '');
+		if (hosts[0] !== '[')
+			hosts = '[' + hosts + ']';
+
+		ret.push (name + ' - `' + hosts + '`');
+	}
+});
+
+module.exports = ret.join ('\n- ');
+
+// console.info (module.exports);
