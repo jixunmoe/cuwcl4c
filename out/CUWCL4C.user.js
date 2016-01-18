ad5af80cb38b364defc883ccefa5dade48cfb77e
JixunMoe <Jixun.Moe@Gmail.Com>
2016-01-18 23:03:32 +0000

    minor changes.

--- out/CUWCL4C.user.js
+++ out/CUWCL4C.user.js
@@ -43,7 +43,7 @@
 
 // @author         Jixun.Moe<Yellow Yoshi>
 // @namespace      http://jixun.org/
-// @version        3.0.511
+// @version        3.0.512
 
 // 全局匹配
 // @include http://*
@@ -1412,9 +1412,7 @@ H.extract(function () { /*
 	generateCdn: function () {
 		// TODO:
 		// + 用户提供 CDN 列表
-		// + 用户指定使用的 IP 缓存节点
-		H.log('%c海外模式启动', 'font-size:10em;color:#fff;text-shadow:0 1px 0#ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15)');
-
+		// + 用户指定使用的 IP 缓存节点 (好像会随机?)
 		var cdns = [];
 		var cdnList = {
 			// 山东电信
